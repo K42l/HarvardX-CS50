140e774e749951199ee5e65d99404b62a7c1119c
K42l <kg.lino325@gmail.com>
2024-02-29 13:48:04 -0300

    Bootstrap and amount
    Added some bootstrap so it doesn't look so plain and add a option to type in a amount  to convert the currency

--- JavaScript/currency.js
+++ JavaScript/currency.js
@@ -27,43 +27,59 @@ document.addEventListener("DOMContentLoaded", () => {
     //})
     const apiKey = document.querySelector("#apiKey")
     document.querySelector("form").onsubmit = () => {
-        document.querySelector("#apiReturn").style.color = "Red"
-        if(base.value !== "" && currency.value !== ""){
-            if(apiKey.value !== ""){
-                //There are two GET options for the free API user
-                //"https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/BASE-CURENCY" that i'm using in this example
-                //And "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/BASE-CURRENCY/TARGET-CURRENCY"
-                //The second one will return the converted value for the target currency provided
-                //I used the first one to get closer to the example provided in the Harvard edx course
-                fetch(`https://v6.exchangerate-api.com/v6/${apiKey.value}/latest/${base.value}`)
-                .then(response => response.json())
-                .then(data =>{
-                    if(data.result === "success"){
-                        const rate = data.conversion_rates[currency.value];
-                        console.log(rate)
-                        if(rate !== undefined){
-                            const baseTxt = base.options[base.selectedIndex].text
-                            const currencyTxt = currency.options[currency.selectedIndex].text
-                            document.querySelector("#apiReturn").style.color = "Black"
-                            document.querySelector("#apiReturn").innerHTML = `1 ${baseTxt} is equal to ${rate.toFixed(3)} ${currencyTxt}`
-                        }
-                    } else {
-                        if(data["error-type"] === "invalid-key"){
-                            document.querySelector("#apiReturn").innerHTML = "API key is invalid. Please provide a valid API Key"
+        const apiReturn =  document.querySelector("#apiReturn")
+        apiReturn.style.display = "none"
+        apiReturn.classList.remove("alert-success");
+        apiReturn.classList.add("alert");
+        apiReturn.classList.add("alert-danger");
+
+        const amount = document.querySelector("#amount")
+        if(apiKey.value !== ""){
+            if(base.value !== "" && currency.value !== ""){
+                if(!isNaN(parseFloat(amount.value))){
+                    //There are two GET options for the free API user
+                    //"https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/BASE-CURENCY" that i'm using in this example
+                    //And "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/BASE-CURRENCY/TARGET-CURRENCY"
+                    //The second one will return the converted value for the target currency provided
+                    //I used the first one to get closer to the example provided in the Harvard edx course
+                    fetch(`https://v6.exchangerate-api.com/v6/${apiKey.value}/latest/${base.value}`)
+                    .then(response => response.json())
+                    .then(data =>{
+                        if(data.result === "success"){
+                            const rate = data.conversion_rates[currency.value];
+                            console.log(rate)
+                            if(rate !== undefined){
+                                const baseTxt = base.options[base.selectedIndex].text
+                                const currencyTxt = currency.options[currency.selectedIndex].text
+                                apiReturn.innerHTML = `${amount.value} ${baseTxt} is equal to ${(rate * amount.value).toFixed(3) } ${currencyTxt}`
+                                apiReturn.classList.remove("alert-danger");
+                                apiReturn.classList.add("alert-success");
+                                apiReturn.style.display = "block"
+                            }
                         } else {
-                            document.querySelector("#apiReturn").innerHTML = `Error Type: ${data["error-type"]}`
+                            if(data["error-type"] === "invalid-key"){
+                                apiReturn.innerHTML = "API key is invalid. Please provide a valid API Key"
+                            } else {
+                                apiReturn.innerHTML = `Error Type: ${data["error-type"]}`
+                            }
+                            apiReturn.style.display = "block"
                         }
-                    }
-                })
-                .catch(err => {
-                    console.log(err)
-                    document.querySelector("#apiReturn").innerHTML = err
-                })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        apiReturn.innerHTML = err
+                    })
+                } else {
+                    apiReturn.innerHTML = "Please type in a valid amount (Use '.' for decimal)"
+                    apiReturn.style.display = "block"
+                }        
             } else {
-                document.querySelector("#apiReturn").innerHTML = "Please provide a API Key"
+                apiReturn.innerHTML = "Please select the currencies you want to convert"
+                apiReturn.style.display = "block"
             }
         } else {
-            document.querySelector("#apiReturn").innerHTML = "Please select the currencies you want to convert"
+            apiReturn.innerHTML = "Please provide a API Key"
+            apiReturn.style.display = "block"
         }
         return false;
     }
